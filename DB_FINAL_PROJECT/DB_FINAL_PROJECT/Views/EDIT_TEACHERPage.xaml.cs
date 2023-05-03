24752351f031bc735507da08cf20a08a0ac26987
Bilal Tariq <hafizbilaltariq360@gmail.com>
2023-05-04 00:12:51 +0500

    All user inputs done from user

--- DB_FINAL_PROJECT/DB_FINAL_PROJECT/Views/EDIT_TEACHERPage.xaml.cs
+++ DB_FINAL_PROJECT/DB_FINAL_PROJECT/Views/EDIT_TEACHERPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DB_FINAL_PROJECT.ViewModels;
-
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using static DB_FINAL_PROJECT.App;
 
 namespace DB_FINAL_PROJECT.Views;
 
@@ -15,5 +16,85 @@ public sealed partial class EDIT_TEACHERPage : Page
     {
         ViewModel = App.GetService<EDIT_TEACHERViewModel>();
         InitializeComponent();
+        LoadOnPage();
+    }
+
+    private void LoadOnPage()
+    {
+        if (LoginPortal.LoginAdd)
+        {
+            Visible1.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void SearchButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void UpdateButton_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void GenderMale_Click(object sender, RoutedEventArgs e)
+    {
+        genText.Content = "Male";
+    }
+
+    private void GenderFemale_Click(object sender, RoutedEventArgs e)
+    {
+        genText.Content = "Female";
+    }
+
+    private void GenderOther_Click(object sender, RoutedEventArgs e)
+    {
+        genText.Content = "Other";
+    }
+
+    private void BGAPositive_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "A+";
+    }
+
+    private void BGANegative_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "A-";
+    }
+
+    private void BGBPositive_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "B+";
+    }
+
+    private void BGBNegative_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "B-";
+    }
+
+    private void BGABPositive_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "AB+";
+    }
+
+    private void BGABNegative_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "AB-";
+    }
+
+    private void BGOBPositive_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "O+";
+    }
+
+    private void BGOBNegative_Click(object sender, RoutedEventArgs e)
+    {
+        bgText.Content = "O-";
+    }
+    private void FeePaid_Click(object sender, RoutedEventArgs e)
+    {
+        feeText.Content = "Paid";
+    }
+    private void FeeUnpaid_Click(object sender, RoutedEventArgs e)
+    {
+        feeText.Content = "Unpaid";
     }
 }
